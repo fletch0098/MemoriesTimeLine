758dcfd1f4848cc6f78b495c102dddc0776a85c3
Brian Fletcher <brian.fletcher@arkanosoft.com>
2018-07-26 10:57:37 -0400

    Angular .net core setup complete

--- MTL.AngularUI/Program.cs
+++ MTL.AngularUI/Program.cs
@@ -19,6 +19,28 @@ namespace MTL.AngularUI
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+
+                .UseStartup<Startup>()
+
+                //Appsetting
+                .ConfigureAppConfiguration((webHostBuilderContext, configurationbuilder) =>
+                {
+                    var environment = webHostBuilderContext.HostingEnvironment;
+                    string commonSettingFileName = "CommonSettings.json";
+                    if (environment.EnvironmentName == "Development")
+                    {
+                        commonSettingFileName = "CommonSettings.Development.json";
+                    }
+
+                    string pathOfCommonSettingsFile = Path.Combine(environment.ContentRootPath, "..", @"MTL.Library\Common");
+                    string pathOfProdCommonSettingsFile = Path.Combine(environment.ContentRootPath, @"Common");
+                    configurationbuilder
+                            .AddJsonFile("appSettings.json", optional: true)
+                            .AddJsonFile(Path.Combine(pathOfCommonSettingsFile, commonSettingFileName), optional: true)
+                            .AddJsonFile(Path.Combine(pathOfProdCommonSettingsFile, commonSettingFileName), optional: true)
+                            .AddJsonFile($"appsettings.{environment.EnvironmentName}.json", optional: true);
+
+                    configurationbuilder.AddEnvironmentVariables();
+                });
     }
 }
--- MTL.AngularUI/Startup.cs
+++ MTL.AngularUI/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using MTL.Library.Common;
 
 namespace MTL.AngularUI
 {
@@ -20,6 +21,12 @@ namespace MTL.AngularUI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var appSettings = Configuration.GetSection("AppSettings");
+            services.Configure<AppSettings>(appSettings);
+
+            var constants = Configuration.GetSection("Constants");
+            services.Configure<Constants>(constants);
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             // In production, the Angular files will be served from this directory
